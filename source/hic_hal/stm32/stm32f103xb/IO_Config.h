f1026051d74148227f278b02c7895927c8353817
1Deming <remember_smile@outlook.com>
2020-04-22 13:13:55 +0800

    modify LED PIN

--- source/hic_hal/stm32/stm32f103xb/IO_Config.h
+++ source/hic_hal/stm32/stm32f103xb/IO_Config.h
@@ -37,9 +37,12 @@ COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F103XB);
 #define USB_CONNECT_OFF()            (USB_CONNECT_PORT->BRR  = USB_CONNECT_PIN)
 
 //Connected LED
-#define CONNECTED_LED_PORT           GPIOA
-#define CONNECTED_LED_PIN            GPIO_PIN_9
-#define CONNECTED_LED_PIN_Bit        9
+//#define CONNECTED_LED_PORT           GPIOA
+//#define CONNECTED_LED_PIN            GPIO_PIN_9
+//#define CONNECTED_LED_PIN_Bit        9
+#define CONNECTED_LED_PORT           GPIOB
+#define CONNECTED_LED_PIN            GPIO_PIN_6
+#define CONNECTED_LED_PIN_Bit        6
 
 //When bootloader, disable the target port(not used)
 #define POWER_EN_PIN_PORT            GPIOB
@@ -74,9 +77,12 @@ COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F103XB);
 #define PIN_HID_LED                  GPIO_PIN_9
 #define PIN_HID_LED_Bit              9
 
-#define PIN_CDC_LED_PORT             GPIOA
-#define PIN_CDC_LED                  GPIO_PIN_9
-#define PIN_CDC_LED_Bit              9
+//#define PIN_CDC_LED_PORT             GPIOA
+//#define PIN_CDC_LED                  GPIO_PIN_9
+//#define PIN_CDC_LED_Bit              9
+#define PIN_CDC_LED_PORT             GPIOB
+#define PIN_CDC_LED                  GPIO_PIN_5
+#define PIN_CDC_LED_Bit              5
 
 #define PIN_MSC_LED_PORT             GPIOA
 #define PIN_MSC_LED                  GPIO_PIN_9
